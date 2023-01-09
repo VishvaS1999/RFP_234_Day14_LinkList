519b7624e423b7d2a4f5c23c8303a93049f2e16b
VishvaS1999 <gvishva2601@gmail.com>
2023-01-09 23:31:06 +0530

    Create LinkedList by Adding 30 and 56 and 70

--- src/com/Linklist/LinkListMain.java
+++ src/com/Linklist/LinkListMain.java
@@ -1,51 +1,64 @@
 package com.Linklist;
-    class Node<T>
-    {
-        T data;
-        Node<T> next;
+class Node<T>
+{
+    T data;
+    Node<T> next;
 
-        public Node(T data)
+    public Node(T data)
+    {
+        this.data=data;
+    }
+}
+class LinkList<T>
+{
+    Node<T> head;
+    Node<T> tail;
+    public void push(T data)
+    {
+        Node<T> newNode = new Node(data);
+        if (head==null)
         {
-            this.data=data;
+            head=newNode;
+            tail=newNode;
+        }
+        else
+        {
+            newNode.next=head;
+            head=newNode;
         }
     }
-    class LinkList<T>
+    public void add(T data)
     {
-        Node<T> head;
-        Node<T> tail;
-
-        public void push(T data)
+        Node<T> newNode = new Node(data);
+        if (head == null)
         {
-            Node<T> newNode = new Node(data);
-            if (head==null)
-            {
-                head=newNode;
-                tail=newNode;
-            }
-            else
-            {
-                newNode.next=head;
-                head=newNode;
-            }
+            head = newNode;
+            tail = newNode;
         }
-        public  void show()
+        else
         {
-            if(head==null)
-            {
-                System.out.println("Link List Empty");
-            }
-            else
-            {
-                Node<T> temp=head;
-                while (temp!=null)
-                {
-                    System.out.println(temp.data + " ");
-                    temp=temp.next;
-                }
-                System.out.println();
-            }
+            tail.next = newNode;
+            tail = newNode;
         }
     }
+    public  void show()
+    {
+        if(head==null)
+        {
+            System.out.println("Link List Empty");
+        }
+        else
+        {
+            Node<T> temp=head;
+            while (temp!=null)
+            {
+                System.out.println(temp.data + " ");
+                temp=temp.next;
+            }
+            System.out.println();
+        }
+    }
+}
     public class LinkListMain
     {
         public static void main(String[] args)
@@ -56,5 +69,11 @@ package com.Linklist;
             list1.push(56);
             list1.show();
             System.out.println();
+
+            LinkList<Integer> list2=new LinkList<>();
+            list2.add(56);
+            list2.add(30);
+            list2.add(70);
+            list2.show();
         }
     }
